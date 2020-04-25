4bf9bf60c96d53a3d67155db4570ee97bde6ddfb
wgz <852043941@qq.com>
2020-04-25 17:09:06 +0800

    万光宗提交

---
+++ src/main/java/com/Personnel/MicroPersonnelApplication.java
@@ -0,0 +1,13 @@
+package com.Personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPersonnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPersonnelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Personnel/MicroPersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPersonnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
