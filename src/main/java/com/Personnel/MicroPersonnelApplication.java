89068b7c825e15d187de04589b5e92ad3633883c
wgz <852043941@qq.com>
2020-04-25 17:14:46 +0800

    wgz

--- src/main/java/com/Personnel/MicroPersonnelApplication.java
+++ src/main/java/com/Personnel/MicroPersonnelApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroPersonnelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroPersonnelApplication.class, args);
+		  SpringApplication.run(MicroPersonnelApplication.class, args);
+		  System.out.println("666");
 	}
 
 }
+   
